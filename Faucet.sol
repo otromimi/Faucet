f0477b5a63aa3083313152973cef3172f4441990
Guillermo Leiro <7454460-otromimi@users.noreply.gitlab.com>
2021-06-23 02:36:08 +0200

    first commit

---
+++ Faucet.sol
@@ -0,0 +1,41 @@
+//pragma solidity ^0.4.19;
+pragma solidity ^0.8.6;
+//SPDX-License-Identifier: UNLICENSED
+
+// Our first contract is a faucet!
+contract Faucet {
+
+    address payable owner;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+            _;
+    }
+
+   // Give out ether to anyone who asks
+   function withdraw(uint withdraw_amount) public {
+
+       // Limit withdrawal amount
+       require(withdraw_amount <= 100000000000000000);
+       
+        // Send the amount to the address that requested it
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+
+    // Accept any incoming amount
+    fallback () external payable {}
+
+    receive() external payable {
+            // React to receiving ether
+        }
+
+    // Destructor
+    function destroy() external onlyOwner {
+        selfdestruct(owner);
+    }
+
+}
